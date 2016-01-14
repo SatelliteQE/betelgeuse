88ea9fa8feb1e57a244cdf1c8999f7c17cdabaf6
Elyézer Mendes Rezende <elyezermr@gmail.com>
2016-01-13 23:43:12 -0200

    Add test for parse_junit
    Make sure that the functionality will not break in the future.

--- tests.py
+++ tests.py
@@ -1,4 +1,34 @@
-from betelgeuse import parse_requirement_name
+from StringIO import StringIO
+from betelgeuse import parse_junit, parse_requirement_name
+
+
+JUNIT_XML = """<testsuite tests="4">
+    <testcase classname="foo1" name="test_passed"></testcase>
+    <testcase classname="foo2" name="test_skipped">
+        <skipped message="Skipped message">...</skipped>
+    </testcase>
+    <testcase classname="foo3" name="test_failure">
+        <failure type="Type" message="Failure message">...</failure>
+    </testcase>
+    <testcase classname="foo4" name="test_error">
+        <error type="ExceptionName" message="Error message">...</error>
+    </testcase>
+</testsuite>
+"""
+
+
+def test_parse_junit():
+    junit_xml = StringIO(JUNIT_XML)
+    assert parse_junit(junit_xml) == [
+        {'classname': 'foo1', 'name': 'test_passed', 'status': 'passed'},
+        {'classname': 'foo2', 'message': 'Skipped message',
+         'name': 'test_skipped', 'status': 'skipped'},
+        {'classname': 'foo3', 'name': 'test_failure',
+         'message': 'Failure message', 'status': 'failure', 'type': 'Type'},
+        {'classname': 'foo4', 'name': 'test_error', 'message': 'Error message',
+         'status': 'error', 'type': 'ExceptionName'}
+    ]
+    junit_xml.close()
 
 
 def test_parse_requirement_name():
@@ -8,5 +38,3 @@ def test_parse_requirement_name():
     ]
     for name in names:
         assert parse_requirement_name(name) == 'Compute Resource'
-
-
