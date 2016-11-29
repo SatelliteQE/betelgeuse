1849eb690d6f99bed1f697654e2ad44f23c4a89c
Elyézer Rezende <elyezermr@gmail.com>
2016-11-29 10:49:27 -0200

    Update sample project
    Add two new tests, one that always fail and the other always is skipped. Update
    the junit xml report based on this change.

--- sample_project/tests/test_login_example.py
+++ sample_project/tests/test_login_example.py
@@ -1,5 +1,8 @@
 # coding=utf-8
-"""Test class for Login"""
+"""Test class for Login
+
+:requirement: Importer Test
+"""
 
 import unittest
 
@@ -17,3 +20,27 @@ class LoginTestCase(unittest.TestCase):
         :expectedresults: User is able to login successfully
         """
         pass
+
+    def test_login_2(self):
+        """This is an expected failure
+
+        :id: 5adbfbe3-9594-46bb-b8b6-d8ef3dbca6b6
+
+        :steps:
+
+            1. First Step
+            2. Second Step
+
+        :expectedresults:
+
+            1. First Result
+            2. Second Result
+        """
+        self.fail('Expected failure')
+
+    def test_login_3(self):
+        """This is an expected skip
+
+        :id: 76fdbb37-1b05-4f90-918e-d34e5e22ed7e
+        """
+        self.skipTest('Expected skip')
