73d90d8ddb685a0ce412717390165bed93e489a8
Elyézer Rezende <elyezermr@gmail.com>
2018-07-18 11:53:54 -0300

    Add sample test case with global requirement

---
+++ sample_project/tests/test_init.py
@@ -0,0 +1,15 @@
+"""Tests for the fields defined on the __init__.py module."""
+
+
+def test_global_requirement(self):
+    """Test case to find if the global requirement will be pulled.
+
+    :id: 3b658fe1-3d96-4ddb-bdf9-2abd950567c7
+
+    :steps: Run Betelgeuse and check if this test case to collect this test
+        case.
+
+    :expectedresults: The test case must have the requirement defined as the
+        Global Requirement from the ``__init__.py`` file.
+    """
+    pass
